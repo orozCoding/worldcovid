5a281f2b73d0272e330ffac228f5d8a339513d90
orozcoding <orozcoding@gmail.com>
2022-03-07 22:04:09 -0400

    Fix singleCountries linters

--- src/components/countries/singleCountry.js
+++ src/components/countries/singleCountry.js
@@ -1,50 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 
 const SingleCountry = (props) => {
   const { country } = props;
+  const {
+    name, date, today_confirmed: confirmed,
+    today_deaths: deaths, today_new_confirmed: todayConfirmed,
+    today_new_deaths: todayDeaths,
+  } = country;
   const navigate = useNavigate();
 
   return (
     <div className="selfCountries d-flex col">
       <div className="countryContainer d-flex col">
-        <div className="selfTitle lato bold d-flex"><p>{country.name}</p></div>
+        <div className="selfTitle lato bold d-flex"><p>{name}</p></div>
         <div className="selfDate d-flex">
           <p className="bold">Date:</p>
-          <p>{country.date}</p>
+          <p>{date}</p>
           {' '}
 
         </div>
         <div className="selfStats">
           <div className="d-flex">
             <p className="bold">Total Cases:</p>
-            <p>{country.today_confirmed}</p>
+            <p>{confirmed}</p>
             {' '}
 
           </div>
           <div className="d-flex">
             <p className="bold">Total Deaths:</p>
-            <p>{country.today_deaths}</p>
+            <p>{deaths}</p>
             {' '}
 
           </div>
           <div className="d-flex">
             <p className="bold">Cases today:</p>
-            <p>{country.today_new_confirmed}</p>
+            <p>{todayConfirmed}</p>
             {' '}
 
           </div>
           <div className="d-flex">
             <p className="bold">Deaths today:</p>
-            <p>{country.today_new_deaths}</p>
+            <p>{todayDeaths}</p>
             {' '}
 
           </div>
         </div>
-        <div className="backButton"><i className="bi bi-arrow-left-circle ck" onClick={() => navigate('/')} /></div>
+        <div className="backButton"><i className="bi bi-arrow-left-circle ck" onClick={() => navigate('/')} aria-hidden="true" /></div>
       </div>
     </div>
   );
+};
+
+SingleCountry.propTypes = {
+  country: PropTypes.shape({
+    name: PropTypes.string,
+    date: PropTypes.string,
+    today_confirmed: PropTypes.number,
+    today_deaths: PropTypes.number,
+    today_new_confirmed: PropTypes.number,
+    today_new_deaths: PropTypes.number,
+  }).isRequired,
 };
 
 export default SingleCountry;
